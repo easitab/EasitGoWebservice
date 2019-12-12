139fbbe352d5f2517f20191d64f1e845049d48ae
Anders Thyrsson <anders.thyrsson@easit.com>
2019-12-12 09:46:48 +0100

    Added test for building xml

---
+++ EasitGoWebservice/tests/New-XMLforEasit.Tests.ps1
@@ -0,0 +1,36 @@
+﻿Describe "New-XMLforEasit" {
+    $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $scriptFolder = Split-Path -Parent $here
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+    . "$scriptFolder\$sut"
+    
+    $scriptContent = Get-Content -Path "$scriptFolder\$sut" -Raw
+    
+    Context 'Building XML' {
+        It 'Should define schemas' {
+            $scriptContent -match '\$xmlnsSch = "http://www.easit.com/bps/schemas"' | Should Be $true
+            $scriptContent -match '\$xmlnsSoapEnv = "http://schemas.xmlsoap.org/soap/envelope/"' | Should Be $true
+        }
+        
+        It 'Should have a declaration' {
+            $scriptContent -match '\[System\.Xml\.XmlDeclaration\] \$xmlDeclaration' | Should Be $true
+            $scriptContent -match '\$payload\.AppendChild\(\$xmlDeclaration\)' | Should Be $true
+        }
+    
+        It 'Should create a envelope element' {
+            $scriptContent -match '\$soapEnvEnvelope = \$payload\.CreateElement\("soapenv\:Envelope"\,\"\$xmlnsSoapEnv"\)' | Should Be $true
+            $scriptContent -match '\$soapEnvEnvelope\.SetAttribute\("xmlns\:sch","\$xmlnsSch"\)' | Should Be $true
+            $scriptContent -match '\$payload\.AppendChild\(\$soapEnvEnvelope\)' | Should Be $true
+        }
+    
+        It 'Should create a header element' {
+            $scriptContent -match '\$soapEnvHeader = \$payload\.CreateElement\(.{1}soapenv\:Header.{1},"\$xmlnsSoapEnv"\)' | Should Be $true
+            $scriptContent -match '\$soapEnvEnvelope\.AppendChild\(\$soapEnvHeader\)' | Should Be $true
+        }
+    
+        It 'Should create a body element' {
+            $scriptContent -match '\$soapEnvBody = \$payload\.CreateElement\("soapenv\:Body","\$xmlnsSoapEnv"\)' | Should Be $true
+            $scriptContent -match '\$soapEnvEnvelope\.AppendChild\(\$soapEnvBody\)' | Should Be $true
+        }
+    }
+}
