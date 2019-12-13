d42c537755be8c6bc0661b84f3fd51ed99fa803a
Anders Thyrsson <anders.thyrsson@easit.com>
2019-12-13 11:34:20 +0100

    Added dynamic to tests

--- EasitGoWebservice/tests/Import-GOContactItem.Tests.ps1
+++ EasitGoWebservice/tests/Import-GOContactItem.Tests.ps1
@@ -1,12 +1,13 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $scriptFolder = Split-Path -Parent $here
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$commandName = $sut -replace '.ps1',''
 . "$scriptFolder\$sut"
 
 #$scriptContent = Get-Content -Path "$scriptFolder\$sut" -Raw
 
 Describe 'Mandatory Params' {
     It 'Should demand an api key' {
-        (((Get-Command -Name Import-GOContactItem).ParameterSets).Parameters | Where-Object -FilterScript {$_.IsMandatory -eq 1 -and $_.Name -eq 'apikey'}).IsMandatory | Should Be $true
+        (((Get-Command -Name "$commandName").ParameterSets).Parameters | Where-Object -FilterScript {$_.IsMandatory -eq 1 -and $_.Name -eq 'apikey'}).IsMandatory | Should Be $true
     }
 }
