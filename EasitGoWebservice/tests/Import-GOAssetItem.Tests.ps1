db2e389744322c5275b94e20ce78ec05ea557842
Easit AB <github@easit.com>
2019-12-13 14:55:34 +0100

    cleanup

--- EasitGoWebservice/tests/Import-GOAssetItem.Tests.ps1
+++ EasitGoWebservice/tests/Import-GOAssetItem.Tests.ps1
@@ -4,8 +4,6 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $commandName = $sut -replace '.ps1',''
 . "$scriptFolder\$sut"
 
-#$scriptContent = Get-Content -Path "$scriptFolder\$sut" -Raw
-
 Describe 'Mandatory Params' {
     It 'Should demand an api key' {
         (((Get-Command -Name "$commandName").ParameterSets).Parameters | Where-Object -FilterScript {$_.IsMandatory -eq 1 -and $_.Name -eq 'apikey'}).IsMandatory | Should Be $true
