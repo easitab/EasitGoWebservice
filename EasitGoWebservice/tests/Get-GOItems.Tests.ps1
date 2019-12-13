e1389c5a0eae6c52cdbbd923ba815fd690adc0f3
Easit AB <github@easit.com>
2019-12-13 15:24:55 +0100

    Corrected gramar

--- EasitGoWebservice/tests/Get-GOItems.Tests.ps1
+++ EasitGoWebservice/tests/Get-GOItems.Tests.ps1
@@ -4,7 +4,7 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $commandName = $sut -replace '.ps1',''
 . "$scriptFolder\$sut"
 
-Describe 'Mandatory Params' {
+Describe 'Mandatory Parameters' {
     It 'Should demand an api key' {
         (((Get-Command -Name "$commandName").ParameterSets).Parameters | Where-Object -FilterScript {$_.IsMandatory -eq 1 -and $_.Name -eq 'apikey'}).IsMandatory | Should Be $true
     }
