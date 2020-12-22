f5e5b777e288afc811b6b080872e67032fe7da26
Easit AB <github@easit.com>
2020-12-22 21:43:50 +0100

    Added new build scripts

---
+++ build/build_appveyor.ps1
@@ -0,0 +1,69 @@
+# This file in it self is not used but only a template.
+# Actual code that will be executed lives in appveyor.yml
+
+New-Module -Name "$env:moduleName" -ScriptBlock {
+    $modulePath = Join-Path -Path "$env:moduleRoot" -ChildPath "$env:moduleName.psm1"
+    $privScripts = Get-ChildItem -Path "$env:sourceRoot\private" -Filter "*.ps1" -Recurse
+    $pubScripts = Get-ChildItem -Path "$env:sourceRoot\public" -Filter "*.ps1" -Recurse
+    if (!(Test-Path -Path $modulePath)) {
+        $newModuleDir = New-Item -Path "$env:resourceRoot" -Name "$env:moduleName" -ItemType 'directory' -Force
+        $newModuleFile = New-Item -Path "$env:moduleRoot" -Name "$env:moduleName.psm1" -ItemType "file"
+    }
+    foreach ($privateScript in $privScripts) {
+        $scriptContent = Get-Content -Path "$($privateScript.FullName)" -Raw
+        if (Test-Path -Path $modulePath) {
+            Add-Content -Path $modulePath -Value $scriptContent
+        } else {
+            Write-Output "Unable to find modulePath at $modulePath" -ForegroundColor Red
+        }
+    }
+    foreach ($publicScript in $pubScripts) {
+        $scriptContent = Get-Content -Path "$($publicScript.FullName)" -Raw
+        if (Test-Path -Path $modulePath) {   
+            Add-Content -Path $modulePath -Value $scriptContent                     
+            Add-Content -Path $modulePath -Value "Export-ModuleMember -Function $($publicScript.BaseName)"
+        } else {
+            Write-Output "Unable to find modulePath at $modulePath" -ForegroundColor Red
+        }
+    }
+} | Out-Null
+$moduleFilePath = Join-Path -Path "$env:moduleRoot" -ChildPath "${env:moduleName}.psm1"
+if (Test-Path -Path "$moduleFilePath") {
+    Write-Host "Check for moduleFilePath, OK!" -ForegroundColor Green
+}
+else {
+    throw "Unable to find $moduleFilePath"
+}
+$manifestFilePath = Join-Path -Path "$env:moduleRoot" -ChildPath "${env:moduleName}.psd1"
+$manifest = @{
+    Path              = "$manifestFilePath" 
+    RootModule        = "$env:moduleName.psm1" 
+    CompanyName       = "$env:companyName"
+    Author            = "$env:moduleAuthor"
+    ModuleVersion     = "$env:APPVEYOR_BUILD_VERSION"
+    HelpInfoUri       = "$env:helpInfoUri"
+    LicenseUri        = "$env:licenseUri"
+    ProjectUri        = "$env:projectUri"
+    Description       = 'Module to be used with Easit BPS & Easit GO webservice API'
+    PowerShellVersion = '5.1'
+    Copyright         = "(c) 2020 $env:companyName. All rights reserved."
+}
+New-ModuleManifest @manifest | Out-Null
+if (Test-Path -Path "$manifestFilePath") {
+    Write-Host "Check for manifestFilePath, OK!" -ForegroundColor Green
+} else {
+    throw "Unable to find $manifestFilePath"
+}
+
+#################################################################################################
+
+Install-Module Pester -Force
+Invoke-Pester
+
+#################################################################################################
+
+if (Test-ModuleManifest -Path "$manifestFilePath") {
+    $moduleRootPath = Resolve-Path "$env:moduleRoot"
+    Publish-Module -Path "$moduleRootPath" -NuGetApiKey "$env:galleryPublishingKey"
+    Write-Host "Module published!" -ForegroundColor Green
+}
---
+++ build/tempbuild_github.ps1
@@ -0,0 +1,59 @@
+# Runtime variables
+$projectRoot = Split-Path -Path $PSScriptRoot -Parent
+$sourceRoot = Join-Path -Path "$projectRoot" -ChildPath 'source'
+$tempModuleFileName = 'EasitGoWebservice'
+$tempModuleRoot = Join-Path -Path "$projectRoot" -ChildPath "$tempModuleFileName"
+$tempModulePath = Join-Path -Path "$tempModuleRoot" -ChildPath "${tempModuleFileName}.psm1"
+# Runtime variables
+Set-Location -Path $projectRoot
+Write-Output "New module start"
+New-Module -Name "$tempModuleFileName" -ScriptBlock {
+    $projectRoot = Split-Path -Path $PSScriptRoot -Parent
+    $sourceRoot = Join-Path -Path "$projectRoot" -ChildPath 'source'
+    $tempModuleFileName = 'EasitGoWebservice'
+    $allScripts = Get-ChildItem -Path "$sourceRoot" -Filter "*.ps1" -Recurse
+    $tempModuleRoot = Join-Path -Path "$projectRoot" -ChildPath "$tempModuleFileName"
+    $tempModulePath = Join-Path -Path "$tempModuleRoot" -ChildPath "${tempModuleFileName}.psm1"
+    if (!(Test-Path -Path $tempModuleRoot)) {
+        New-Item -Path "$projectRoot" -Name "$tempModuleFileName" -ItemType "directory" | Out-Null
+        Write-Output "Created $tempModuleRoot"
+    }
+    if (!(Test-Path -Path $tempModulePath)) {
+        $tempModuleFile = New-Item -Path "$tempModuleRoot" -Name "${tempModuleFileName}.psm1" -ItemType "file"
+        Write-Output "Created $newModuleFile"
+    }
+    foreach ($script in $allScripts) {
+        $exportFunction = "Export-ModuleMember -Function $($script.BaseName)"
+        $scriptContent = Get-Content -Path "$($script.FullName)" -Raw
+        if (Test-Path -Path $tempModulePath) {    
+            try {
+                Add-Content -Path $tempModuleFile -Value $scriptContent -ErrorAction Stop
+            } catch {
+                Write-Error $_
+                break
+            }
+            try {
+                Add-Content -Path $tempModuleFile -Value $exportFunction -ErrorAction Stop
+            } catch {
+                Write-Error $_
+                break
+            }
+        }
+    }
+}
+Write-Output "New module end"
+$projectUriRoot = 'https://github.com/easitab/EasitGoWebservice'
+$manifest = @{
+    Path              = "$tempModuleRoot/${tempModuleFileName}.psd1" 
+    RootModule        = "$tempModuleRoot/${tempModuleFileName}.psm1" 
+    CompanyName       = "Easit AB"
+    Author            = "Anders Thyrsson"
+    ModuleVersion     = "0.0.1"
+    HelpInfoUri       = "$projectUriRoot/tree/development/docs/v1"
+    LicenseUri        = "$projectUriRoot/blob/development/LICENSE"
+    ProjectUri        = "$projectUriRoot"
+    Description       = 'Module to be used with Easit BPS & Easit GO webservice API'
+    PowerShellVersion = '5.1'
+    Copyright         = "(c) 2021 Easit AB. All rights reserved."
+}
+New-ModuleManifest @manifest
