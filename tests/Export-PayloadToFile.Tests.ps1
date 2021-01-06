609a272828000694215f79147863389f6a8a9bb8
Easit AB <github@easit.com>
2021-01-06 17:58:54 +0100

    Added tests for private functions

---
+++ tests/Export-PayloadToFile.Tests.ps1
@@ -0,0 +1,20 @@
+BeforeAll {
+    $testFilePath = $PSCommandPath.Replace('.Tests.ps1','.ps1')
+    $codeFileName = Split-Path -Path $testFilePath -Leaf
+    $commandName = ((Split-Path -Leaf $PSCommandPath) -replace '.ps1','') -replace '.Tests', ''
+    $testRoot = Split-Path -Path $PSCommandPath -Parent
+    $projectRoot = Split-Path -Path $testRoot -Parent
+    $sourceRoot = Join-Path -Path "$projectRoot" -ChildPath "source"
+    $codeFile = Get-ChildItem -Path "$sourceRoot" -Include "$codeFileName" -Recurse
+    if (Test-Path $codeFile) {
+        . $codeFile
+    } else {
+        Write-Output "Unable to locate code file to test against!" -ForegroundColor Red
+    }
+}
+Describe 'Export-PayloadToFile' {
+    It 'should demand and only accept XML object' {
+        Get-Command "$commandName" | Should -HaveParameter Payload -Type xml
+        Get-Command "$commandName" | Should -HaveParameter Payload -Mandatory
+    }
+}
